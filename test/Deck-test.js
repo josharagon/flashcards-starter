b8832fff0cd7d760335208ba9bae693f3e217afc
Josh Aragon <josharagon99@gmail.com>
2021-02-03 08:53:12 -0700

    Complete deck-test.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,4 +1,25 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
+
+describe('Deck', function(){
+    
+    it('should be a function', function(){
+        const deck = new Deck();
+        expect(Deck).to.be.a('function')
+    });
+
+    it('should take in the entire card deck', function(){
+        const cards = [new Card(), new Card(), new Card()];
+        const deck = new Deck(cards)
+        expect(deck.cards).to.be.a('array');
+    });
+
+    it('should be able to count the cards', function(){
+        const cards = [new Card(), new Card(), new Card()];
+        const deck = new Deck(cards);
+        expect(deck.countCards()).to.equal(3)
+    });
+})
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,36 +9,36 @@ describe('Turn', function() {
     it('should be a function', function() {
         const turn = new Turn();
         expect(Turn).to.be.a('function');
-    })
+    });
 
     it('should take in a guess and a flashcard', function(){
         const card = new Card(1, 'What is my favorite animal', ['dog', 'cat', 'tiger'], 'polar bear')
         const turn = new Turn('my guess', card)
         expect(turn.guess).to.equal('my guess')
         expect(turn.card).to.be.a('object')
-    })
+    });
 
     it('should be able to return the user guess', function() {
         const card = new Card(1, 'What is my favorite animal', ['dog', 'cat', 'tiger'], 'polar bear')
         const turn = new Turn('dog', card)
         expect(turn.returnGuess()).to.equal('dog')
-    })
+    });
 
     it('should be able to return the flashcard', function(){
         const card = new Card(1, 'What is my favorite animal', ['dog', 'cat', 'tiger'], 'polar bear')
         const turn = new Turn('cat', card)
         expect(turn.returnCard()).to.be.an.instanceof(Card)
-    })
+    });
 
     it('should be able to return a boolean value if the user’s guess matches the correct answer on the card', function() {
         const card = new Card(1, 'What is my favorite animal', ['dog', 'cat', 'tiger'], 'polar bear')
         const turn = new Turn('cat', card)
         expect(turn.evaluateGuess()).to.equal(false)
-    })
+    });
 
     it('should tell the user if they are correct or not', function(){
         const card = new Card(1, 'What is my favorite animal', ['dog', 'cat', 'tiger'], 'polar bear')
         const turn = new Turn('polar bear', card)
         expect(turn.giveFeedback()).to.equal('correct!')
-    })
-})
+    });
+});
